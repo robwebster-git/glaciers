64f2be523f0ad04be9ed2be4d5c218e5c5f5f0db
Rob Webster <robwebster@rwebster-laptop.local>
2020-04-13 11:55:28 +0100

    tidy up initial basic find and download code

--- glaciers.py
+++ glaciers.py
@@ -24,19 +24,11 @@ def s1(aoi, from_date, to_date, download):
     '''
     Finds online Sentinel-1 imagery
     '''
-    products = api.query(aoi, date=(from_date, to_date), platformname='Sentinel-1', producttype='GRD', sensoroperationalmode='IW')
 
-    # Creates Pandas dataframe from results of api query
-    df = api.to_dataframe(products)
+    products = api.query(aoi, date=(from_date, to_date), platformname='Sentinel-1', producttype='GRD', sensoroperationalmode='IW')
 
     # Creates GeoPandas GeoDataFrame with the metadata of the scenes and the footprints as geometries
     geodf = api.to_geodataframe(products)
-
-    #print(df.columns)
-    #print(geodf.columns)
-
-    #print(geodf['producttype'])
-    #print(geodf['title'])
 
     print(f'\nSentinel-1 Images from {from_date} to {to_date}:\n')
     
@@ -56,19 +48,11 @@ def s2(aoi, from_date, to_date, download):
     '''
     Finds online Sentinel-2 imagery
     '''
+
     products = api.query(aoi, date=(from_date, to_date), platformname='Sentinel-2', cloudcoverpercentage=(0, 10), producttype='S2MSI2A')
 
-    # Creates Pandas dataframe from results of api query
-    df = api.to_dataframe(products)
-
-    # Creates GeoPandas GeoDataFrame with the metadata of the scenes and the footprints as geometries
+    # Creates GeoDataFrame
     geodf = api.to_geodataframe(products)
-
-    #print(df.columns)
-    #print(geodf.columns)
-
-    #print(geodf['producttype'])
-    #print(geodf['title'])
 
     print(f'\nSentinel-2 Images from {from_date} to {to_date}:\n')
     
@@ -77,8 +61,7 @@ def s2(aoi, from_date, to_date, download):
             print(f"    Product: {api.get_product_odata(id)['title']}  (Online: {api.get_product_odata(id)['Online']})")
 
     else:
-        print('    No valid data in this time period\n')
-
+        print('     No valid data in this time period\n')
 
     if download:
         api.download_all(products, s2dir)   
